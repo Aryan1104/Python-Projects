70ceb9acd9db4b095ce143afad22482632002d4c
Aryan Mishra <123317319+Aryan1104@users.noreply.github.com>
2024-07-05 00:15:42 +0530

    Python.py

---
+++ Python.py
@@ -0,0 +1,22 @@
+//'''
+//Text To Speech
+//-------------------------------------------------------------
+//pip install nltk newspaper3k gtts
+//'''
+
+
+import nltk
+from newspaper import Article
+from gtts import gTTS
+
+
+def text_to_speech(url):
+   article = Article(url)
+   article.download()
+   article.parse()
+   nltk.download('punkt')
+   article.nlp()
+   article_text = article.text
+   language = 'en'
+   my_obj = gTTS(text=article_text, lang=language, slow=False)
+   my_obj.save("read_article.mp3")
